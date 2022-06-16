9b0e5109f5153e7878477daf75ba598e6147b174
KweonMo Koo <mikekoo2010@gmail.com>
2022-06-16 03:16:36 -0700

    Update config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,43 +12,43 @@ const gatsbyRequiredRules = path.join(
 
 module.exports = {
   siteMetadata: {
-    title: `Michael Koo's Website`,
-    description: `Welcome, I am Michael Koo. `,
-    author: `@kooma93`,
-    siteUrl: `https://michaelkoo.dev/`,
+    title: 'Michael Koo\'s Website',
+    description: 'Welcome, I am Michael Koo. ',
+    author: '@kooma93',
+    siteUrl: 'https://michaelkoo.dev/',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-image',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/mk-favicon.png`, // This path is relative to the root of the site.
+        display: 'minimal-ui',
+        icon: 'src/images/mk-favicon.png', // This path is relative to the root of the site.
       },
     },
     {
-      resolve: `gatsby-plugin-sass`,
+      resolve: 'gatsby-plugin-sass',
       options: {
         postCssPlugins: [
-          require("tailwindcss"),
-          require("./tailwind.config.js"), // Optional: Load custom Tailwind CSS configuration
+          require('tailwindcss'),
+          require('./tailwind.config.js'), // Optional: Load custom Tailwind CSS configuration
         ],
       },
     },
@@ -65,18 +65,20 @@ module.exports = {
         // ...
       },
     },
-    `gatsby-plugin-postcss`,
+    'gatsby-plugin-postcss',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     {
-      resolve: `gatsby-plugin-google-gtag`,
+      resolve: 'gatsby-plugin-google-gtag',
       options: {
         trackingIds: [process.env.GATSBY_GA_TAG],
         pluginConfig: {
-          origin: "https://michaelkoo.dev",
+          // Puts tracking script in the head instead of the body
+          head: true,
+          origin: 'https://michaelkoo.dev',
         },
       },
     },
   ],
-}
+};
