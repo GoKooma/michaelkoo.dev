03a5b8b7a74b77bc575f1d1659b4a0f010341cbf
KweonMo Koo <mikekoo2010@gmail.com>
2022-06-19 03:34:59 -0700

    Create children proptype interface

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -13,7 +13,11 @@ import Header from '../Header';
 import Footer from '../Footer';
 import './style.css'; // eslint-disable-line
 
-const Layout = ({ children }) => {
+interface ChildrenProps {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<ChildrenProps> = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
